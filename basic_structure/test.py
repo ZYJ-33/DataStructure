ef74cfcc14e46dd32b485fc7aa81dfb48df7c4e2
ZYJ-33 <jobsera@163.com>
2019-05-03 01:19:58 +0800

    bubblesort selectsort insertsort quicksort heapsort mergesort

---
+++ basic_structure/test.py
@@ -0,0 +1,9 @@
+a = [0, 1, 1, 2, 3, 3, 7, 7, 7, 8, 9, ]
+b = a[0:3]
+b.append(5)
+print(b)
+print(a)
+print(a is b)
+
+l = r = 0
+print(l,r)
---
+++ heap.py
@@ -0,0 +1,119 @@
+from basic_structure.vector import mylist
+from random import randint
+
+class heapnode():
+    ...
+
+class heap(mylist):
+    @staticmethod
+    def parentof(rank):
+        return int((rank - 1) / 2)
+
+    @staticmethod
+    def rightchild(rank):
+        return (rank+1)*2
+
+    @staticmethod
+    def leftchild(rank):
+        return rank*2+1
+
+    def have_rightchild(self, rank, n):
+        return (rank+1)*2 < n
+
+    def have_leftchild(self, rank, n):
+        return rank*2+1 < n
+
+    def greater(self, left, right):
+        if self[left] > self[right]:
+            return left
+        else:
+            return right
+
+    def insert(self, value):
+        self.append(value)
+        self.percolateup(len(self)-1)
+
+    def percolateup(self, rank):
+        upper = self[rank]
+        pr = self.parentof(rank)
+        while self[pr] < upper and rank > 0:
+            self[rank] = self[pr]
+            rank = pr
+            pr = self.parentof(rank)
+        self[rank] = upper
+
+    def heaplify_up_version(self, other):
+        self.clear()
+        for item in other:
+            self.insert(item)
+        del other
+
+    def heaplify_self(self):
+        lo = 1
+        hi = len(self)
+        while lo < hi:
+            self.percolateup(lo)
+            lo += 1
+
+
+    def heap_sort(self):
+        self.heaplify_self()
+        last = len(self)-1
+        while last > 0:
+            self.swap(0, last)
+            self.percolatedown(0, last)
+            last -= 1
+
+
+
+
+    def percolatedown(self, rank, n):
+        downer = self[rank]
+        posible = self.proper_parent(rank, downer, n)
+        while posible is not None:
+            self[rank] = self[posible]
+            rank = posible
+            posible = self.proper_parent(rank, downer, n)
+        self[rank] = downer
+
+
+    def proper_parent(self, rank, downer, n):
+        haverc = self.have_rightchild(rank, n)
+        havelc = self.have_leftchild(rank, n)
+        if havelc and haverc:
+            posible = self.greater(self.leftchild(rank), self.rightchild(rank))
+            if downer > self[posible]:
+                return None
+            return posible
+        elif havelc:
+            posible = self.leftchild(rank)
+            if downer > self[posible]:
+                return None
+            return posible
+        elif haverc:
+            posible = self.rightchild(rank)
+            if downer > self[posible]:
+                return None
+            return posible
+        else:
+            return None
+
+
+    def get_max(self):
+        return self[0]
+
+    def set_max(self, index):
+        self[0] = self[index]
+
+    def del_max(self):
+        max = self.get_max()
+        self.set_max(len(self)-1)
+        self.percolatedown(0)
+        return max
+
+
+
+if __name__ == "__main__":
+    h = heap([randint(0, 100000)for i in range(100000)])
+    h.heap_sort()
+    print(h)
